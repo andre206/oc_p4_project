8fe7a31b755abb716de9d4b28d6edb3aa4b3f500
maticha84 <maticha84@gmail.com>
2021-06-17 19:13:04 +0200

    modification structure de base

---
+++ models/player.py
@@ -0,0 +1,9 @@
+""" Ici les informations pour la création des joueurs.
+Doit contenir :
+- nom de famille
+- prénom
+- Date de naissance
+- sexe
+- classement (un chiffre positif)
+- un numéro d'instance de joueur (indice)
+-faire une vérification si un joueur existe, sinon le créer- pas de gestion de suppression des joueurs pour le moment"""

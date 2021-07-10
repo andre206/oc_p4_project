0bab96c0d42ee23fa4b8fc28941ac0b050cc715d
maticha84 <maticha84@gmail.com>
2021-07-10 17:12:45 +0200

    redaction of correct docstrings - for_tournament.py

--- controllers/for_tournament.py
+++ controllers/for_tournament.py
@@ -1,7 +1,23 @@
 #! /usr/bin/env python3
 # coding: utf-8
 """
-Control specific for new tournament creation
+Control specific for tournament creation and/or modification
+
+Functions
+---------
+control_mane_place_tournament(name_place)
+    check that the name of the tournament place is correctly define
+control_time_control(control_time)
+    check that the control time is correctly define
+control_number_of_players(number_of_players)
+    check that the number of player is correct (even players)
+control_number_of_round(number_of_round, number_of_players)
+    check that the number of round selected by the user is correct
+tournament_in_progress(tournament_table, id_tournament)
+    control that th id of the tournament progress is known
+participants_tournament(tournament, list_of_players, sort_by='score')
+    calculate the current score of the participants of a tournament
+    and to return the list of participants with the updated scores.
 """
 import re
 from controllers.maximum_round import max_rounds_without_duplicate
@@ -9,6 +25,21 @@ from controllers.backup_restore_round import deserialized_round
 
 
 def control_name_place_tournament(name_place):
+    """
+    Check that the name place contains some letters.
+
+    Parameters
+    ----------
+    name_place = str
+        the name place input by the user
+
+    Returns
+    -------
+    0 : int
+        if the name place doesn't match the pattern
+    1 : int
+        if the name place matches pattern
+    """
     name_place_valid = re.search(r"[A-Za-zéùàèêëï\- ]", name_place)
     if name_place_valid is None:
         print("\033[91mInvalid entry\033[0m\n")
@@ -18,6 +49,23 @@ def control_name_place_tournament(name_place):
 
 
 def control_time_control(control_time):
+    """
+    Check that the control time is correctly define
+    the entry must be 1, 2 or 3.
+
+    Parameters
+    ----------
+    control_time : str
+        the control time input by the player. Must be '1', '2', or '3'
+
+    Returns
+    -------
+    choice_control : int
+        if the control time dosen't match pattern
+    choice_control : str
+        if the control time matches patern. Return 'bullet', 'blitz' or
+        'quick hit'
+    """
     control_time_valid = re.search(r'^[123]$', control_time)
     choice_control = 0
     if control_time_valid is None:
@@ -32,7 +80,53 @@ def control_time_control(control_time):
     return choice_control
 
 
+def control_number_of_players(number_of_players):
+    """
+    Check that the number of players charged by the user is correct and even
+
+    Parameters
+    ----------
+    number_of_players : str
+        input by the user
+
+    Returns
+    -------
+    0 : int
+        if number of players is not valid
+    1 : int
+        if number of players is valid
+    """
+    number_valid = re.search(r'^[\d]{1,3}$', number_of_players)
+    if number_valid is None:
+        return 0
+    elif int(number_of_players) % 2 != 0:  # Must be a multiple of 2 for making matches
+        print("\033[91mThe number of players must be even to play the matches\033[0m\n")
+        return 0
+    else:
+        return 1
+
+
 def control_number_of_round(number_of_round, number_of_players):
+    """
+    Check that the number of rounds charged by the user is correct,
+    and that it does not exceed the maximum number of rounds possible
+    depending on the number of players in the tournament.
+
+    Parameters
+    __________
+    number_of_round : str
+        number input by the user
+    number_of_players : str
+        number input by the user
+
+    Returns
+    -------
+    0 : int
+        if number of rounds is not valid
+    1 : int
+        if number of rounds is valid
+
+    """
     number_valid = re.search(r'^[\d]{1,3}$', number_of_round)
 
     if number_valid is None:
@@ -46,18 +140,25 @@ def control_number_of_round(number_of_round, number_of_players):
         return 1
 
 
-def control_number_of_players(number_of_players):
-    number_valid = re.search(r'^[\d]{1,3}$', number_of_players)
-    if number_valid is None:
-        return 0
-    elif int(number_of_players) % 2 != 0:  # Must be a multiple of 2 for making matches
-        print("\033[91mThe number of players must be even to play the matches\033[0m\n")
-        return 0
-    else:
-        return 1
-
-
 def tournament_in_progress(tournament_table, id_tournament):
+    """
+    control that th id of the tournament progress is known
+
+    Parameters
+    ----------
+    tournament_table : list
+        list of tournament registered in the base
+    id_tournament : str
+        input by the user
+
+    Returns
+    -------
+    tournament : Tournament()
+        an instance of the Tournament class. Corresponding to the id tournament input by the user
+        if known in the base
+    False : binary
+        if the ID doesn't correspond with a known id tournament
+    """
     for tournament in tournament_table:
         if tournament.id_tournament == int(id_tournament):
             return tournament
@@ -66,6 +167,24 @@ def tournament_in_progress(tournament_table, id_tournament):
 
 
 def participants_tournament(tournament, list_of_players, sort_by='score'):
+    """
+    calculate the current score of the participants of a tournament
+    and to return the list of participants with the updated scores.
+
+    Parameters
+    ----------
+    tournament : Tournament
+        an instance of tournament class
+    list_of_players : list
+        a list of players
+    sort_by : str
+        for select the method to sort players. By score by default. Possibly by name.
+
+    Returns
+    -------
+    applicants : list
+        a list of participants tournament, sorted, with update scores.
+    """
     applicants = []
     list_round = deserialized_round(tournament.list_of_round)
 

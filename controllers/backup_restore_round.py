26f5887e3a46343004ab65519d13202fb515871d
maticha84 <maticha84@gmail.com>
2021-07-10 16:29:37 +0200

    redaction of correct docstrings - backup_restore_round.py

--- controllers/backup_restore_round.py
+++ controllers/backup_restore_round.py
@@ -1,18 +1,26 @@
 #! /usr/bin/env python3
 # coding: utf-8
 """
-Permet de sérialiser un round pour l'intégrer dans un tournoi,
-pour sauvegarder et réimporter les données stockées dans
-la mini base de donnée (utilisation de TinyDB et stockage file json)
+This module allows to serialise/deserialise rounds,
+to save and re-import data stored in the
+the mini-database (using TinyDB and json file storage)
 """
 from models.round import Round
 
 
 def serialized_round(a_round):
     """
-    pour sérialiser un round
-    :param a_round: an instance of the Round Class
-    :return: a dictionnary with the information of the tournament
+    for a round serialization
+
+    Parameters
+    ----------
+    a_round: Round
+        an instance of the Round Class
+
+    Returns
+    -------
+    serialized_round_dict : dict
+        a dictionnary with the information of a round
     """
     serialized_round_dict = {
         'name': a_round.name,
@@ -27,9 +35,17 @@ def serialized_round(a_round):
 
 def deserialized_round(list_of_round_dict):
     """
-     recuparation of round from the tournaments table
-    :param round_dict: the round_dict from the db.json
-    :return: a Round()
+     Recuperation of round from the tournaments table
+
+     Parameters
+     ----------
+     list_of_round_dict: dict
+        a list of round_dict from the db.json
+
+    Returns
+    -------
+    list_of_round : list
+        a list of Round()
     """
 
     list_of_round = []

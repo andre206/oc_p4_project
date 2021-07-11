b834c8ca05f2c3cd4669f0f17ade15fff6d37951
maticha84 <maticha84@gmail.com>
2021-07-11 18:16:51 +0200

    redaction of correct docstrings - menu_input.py

--- controllers/menu_input.py
+++ controllers/menu_input.py
@@ -1,12 +1,34 @@
 #! /usr/bin/env python3
 # coding: utf-8
-""" Menus input fonction - """
+"""
+Menus input functions -
+
+Functions
+---------
+choice_option()
+    for input the user option
+selected_tournament(tournament_table)
+    for selected a specific tournament
+
+"""
 from time import sleep
 
 from controllers.user_entry import control_id
 
 
 def choice_option():
+    """
+    test the option input by user
+
+    Raises
+    ------
+    ValueError
+        the input must be an integer
+
+    Returns
+    option : int
+        the option chosen by user
+    """
     try:
         option = int(input("\033[95mEnter your choice : \033[0m"))
     except ValueError:
@@ -17,6 +39,20 @@ def choice_option():
 
 
 def selected_tournament(tournament_table):
+    """
+    for selected a specific tournament
+
+    Parameters
+    ----------
+    tournament_table : tinydb.table.Table
+        tournament table
+
+    Returns
+    -------
+    result : str(int)
+        the id tournament selected by user
+        0 if id not found
+    """
     id_tournament = input("Enter the ID Tournament to modify : ")
     while control_id(id_tournament) == 0:
         id_tournament = input("Enter the ID Tournament to modify : ")
@@ -26,7 +62,7 @@ def selected_tournament(tournament_table):
             result = id_tournament
 
     if result == 0:
-        print('Tournament ID not found. No changes registered')
-        sleep(1)
+        print('\033[91mTournament ID not found. \033[33mNo changes registered\033[0m')
+        sleep(0.5)
 
     return result

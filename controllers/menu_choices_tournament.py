ff2f2bb9567bda36cf1ac9bbc6f63ccc79ba681f
maticha84 <maticha84@gmail.com>
2021-07-13 22:19:54 +0200

    correction calculate rounds - add try except : id0 + id1 id 8 + id7 id 0 + id8

--- controllers/menu_choices_tournament.py
+++ controllers/menu_choices_tournament.py
@@ -425,14 +425,10 @@ class SwitcherModifyTournamentSub(SwitcherMenu):
                                 match_id = (a_match[0][0], a_match[1][0])
                                 match_id = sorted(match_id)
                                 list_of_pairs.append(match_id)
-                        list_of_possible_match, list_matches_played = remove_playing_matches(
-                            list_of_possible_match,
-                            list_of_pairs
-                        )
 
                         this_round_matches = RoundGenerated(
                             list_player_tournament
-                        ).other_round(list_matches_played, sort_by_scores)
+                        ).other_round(list_of_pairs, sort_by_scores)
                         new_round.match_list = this_round_matches
 
                     view_matches_a_round(new_round, list_player_tournament)
